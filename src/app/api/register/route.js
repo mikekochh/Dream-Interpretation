5bb47c76766ed720c8b39a64bb2d23ead145bbda
mikekochh <michaelkoch4444@gmail.com>
2023-11-06 17:20:35 -0500

    Updates to thing

---
+++ src/app/api/register/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import { connectMongoDB } from '../../../../lib/mongodb';
+import User from '../../../../models/user';
+import bcrypt from 'bcryptjs';
+
+export async function POST(req) {
+    try {
+        const { name, email, password } = await req.json();
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        await connectMongoDB();
+        await User.create({ name, email, password:hashedPassword });
+
+        return NextResponse.json({message: "User registered successfully!"}, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({message: "User registration failed!"}, { status: 500 })
+    }
+}
