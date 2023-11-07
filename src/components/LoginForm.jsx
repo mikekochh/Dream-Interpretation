3f09b17dc849f64118b035d6808184210b338f29
mikekochh <michaelkoch4444@gmail.com>
2023-11-06 19:17:58 -0500

    Adding pages folder, saving before trying pages to revert back to

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import validator from 'validator';
+import { signIn } from "next-auth/react";
 
 export default function LoginForm() {
 
@@ -10,27 +11,36 @@ export default function LoginForm() {
     const [error, setError] = useState(""); 
 
     const login = (e) => {
+        e.preventDefault();
+
         if (!email || !password) {
             setError("Please fill in all fields");
-            e.preventDefault();
+            return;
         }
         else if (!validator.isEmail(email)) {
             setError("Please enter a valid email");
-            e.preventDefault();
+            return;
         }
         else {
             setError("");
         }
+
+        try {
+
+        } catch (error) {
+            setError("Login failed!");
+        }
+
     }
 
     return (
         <div className='text-white grid place-items-center h-screen z-0'>
             <div className="p-5 rounded-lg border-t-4 border-white-400 border">
                 <h1 className="text-xl font-bold my-4">Enter Login Details</h1>
-                <form className="flex flex-col gap-3">
+                <form className="flex flex-col gap-3" onSubmit={login}>
                     <input type="text" placeholder="Email" className="LoginInput rounded-lg text-black" onChange={(e) => setEmail(e.target.value)} />
                     <input type="password" placeholder="Password" className="LoginInput rounded-lg text-black" onChange={(e) => setPassword(e.target.value)} />
-                    <Link className="bg-blue-500 rounded-lg py-2 text-white font-bold text-center" href={'/home'} onClick={login}>Login</Link>
+                    <button className="bg-blue-500 rounded-lg py-2 text-white font-bold text-center" onClick={login}>Login</button>
                     { error && (
                         <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
                             {error}
