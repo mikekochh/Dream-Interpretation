d713d981362132d076c942533c1c751966bc5a88
Michael Koch <michaelkoch@Michaels-MBP.fios-router.home>
2023-11-16 20:28:25 -0500

    Update nextauth issue

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,7 +1,7 @@
 import RegisterForm from "@/components/RegisterForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../api/auth/[...nextauth]";
 import StarBackground from "@/components/StarBackground";
 
 export default async function Register() {
