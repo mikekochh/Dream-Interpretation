0bc77d675432ca861d0b8b40e2823fc41698a53e
Michael Koch <michaelkoch@Michaels-MBP.fios-router.home>
2023-11-19 13:56:12 -0500

    More updates

---
+++ src/components/EmailVerificationForm.jsx
@@ -0,0 +1,21 @@
+"use client";
+import ContactAndPrivacyButtons from "./ContactAndPrivacyButtons";
+import { useSearchParams } from 'next/navigation'
+
+export default function EmailVerificationForm() {
+
+    const searchParams = useSearchParams();
+ 
+    const email = searchParams.get('email');
+
+    return (
+        <div className='text-white text-center'>
+            <p className="text-3xl pt-10 text-center">We've sent a verification email to the address of {email}</p>
+            
+            <div className="text-xl pt-5">
+                Please click the link in the email to verify your account.
+            </div>
+            <ContactAndPrivacyButtons />
+        </div>
+    )
+}
