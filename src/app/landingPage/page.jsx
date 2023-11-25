97901d146b2fc71c833bf4ab941a28a68967e9b1
Michael Koch <michaelkoch@Michaels-MBP.fios-router.home>
2023-11-25 14:09:45 -0500

    Beginning to work on landing page

---
+++ src/app/landingPage/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import LandingPageForm from "@/components/LandingPageForm"
+import StarBackground from "@/components/StarBackground";
+
+export default function HomePage() {
+    return (
+        <StarBackground>
+            <LandingPageForm />
+        </StarBackground>
+    )
+}
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -122,45 +122,47 @@ export default function LoginForm() {
     }
 
     return (
-        <div className='text-white grid place-items-center h-screen z-0'>
-            <div className="p-5 rounded-lg border-t-4 border-white-400 border">
-                <h1 className="text-xl font-bold my-4">Enter Login Details</h1>
-                <form className="flex flex-col gap-3" onSubmit={login}>
-                    <input type="text" placeholder="Email" className="LoginInput rounded-lg text-black" onChange={(e) => setEmail(e.target.value)} />
-                    {/* <input type="password" placeholder="Password" className="LoginInput rounded-lg text-black" onChange={(e) => setPassword(e.target.value)} /> */}
-                    <button className="bg-blue-500 rounded-lg py-2 text-white font-bold text-center" onClick={login}>Login</button>
-                    <div className="flex">
-                        { error && (
-                            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2 mr-1">
-                                {error}
-                            </div>
-                        )}
-                        { sendVerifyEmail && (
-                            <div className="bg-blue-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2 ml-1">
-                                <button onClick={sendVerficationEmail} className="underline">
-                                    Send Again?
-                                </button>
-                            </div>
-                        )}
-                        { logginIn && (
-                            <div className="bg-blue-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-                                Loggin in...
-                            </div>
-                        )}
-                        { forgotPassword && (
-                            <div className="bg-blue-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2 ml-1">
-                                <button onClick={sendForgotPassword} className="underline">
-                                    Forgot Password?
-                                </button>
-                            </div>
-                        )}
-                    </div>
-                    <Link href={'/register'} className="text-sm mt-3 text-right">
-                        Don&apos;t have an account? <span className="underline">Register</span>
-                    </Link>
-                </form>
+        <div>
+            <div className='text-white grid place-items-center h-screen z-0'>
+                <div className="p-5 rounded-lg border-t-4 border-white-400 border">
+                    <h1 className="text-xl font-bold my-4">Enter Login Details</h1>
+                    <form className="flex flex-col gap-3" onSubmit={login}>
+                        <input type="text" placeholder="Email" className="LoginInput rounded-lg text-black" onChange={(e) => setEmail(e.target.value)} />
+                        {/* <input type="password" placeholder="Password" className="LoginInput rounded-lg text-black" onChange={(e) => setPassword(e.target.value)} /> */}
+                        <button className="bg-blue-500 rounded-lg py-2 text-white font-bold text-center" onClick={login}>Login</button>
+                        <div className="flex">
+                            { error && (
+                                <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2 mr-1">
+                                    {error}
+                                </div>
+                            )}
+                            { sendVerifyEmail && (
+                                <div className="bg-blue-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2 ml-1">
+                                    <button onClick={sendVerficationEmail} className="underline">
+                                        Send Again?
+                                    </button>
+                                </div>
+                            )}
+                            { logginIn && (
+                                <div className="bg-blue-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+                                    Loggin in...
+                                </div>
+                            )}
+                            { forgotPassword && (
+                                <div className="bg-blue-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2 ml-1">
+                                    <button onClick={sendForgotPassword} className="underline">
+                                        Forgot Password?
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                        <Link href={'/register'} className="text-sm mt-3 text-right">
+                            Don&apos;t have an account? <span className="underline">Register</span>
+                        </Link>
+                    </form>
+                </div>
+                <ContactAndPrivacyButtons />
             </div>
-            <ContactAndPrivacyButtons />
         </div>
     )
 }
