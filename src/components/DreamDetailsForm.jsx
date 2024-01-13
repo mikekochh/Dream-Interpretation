0b73e7f32802191af81a4538b06a18fd6bf2786d
mikekochh <michaelkoch4444@gmail.com>
2024-01-13 12:27:06 -0500

    Updates

--- src/components/DreamDetailsForm.jsx
+++ src/components/DreamDetailsForm.jsx
@@ -29,6 +29,7 @@ export default function DreamsForm() {
             setLoadingInterpretations(true);
             setLoadingNotes(true);
             const res = await axios.get('api/dream/details/' + dreamID);
+            console.log("testing: ", res.data.dreamDetails);
             setDreamDetails(res.data.dreamDetails);
             const resOracles = await axios.get('/api/oracles');
             setOracles(resOracles.data);
@@ -117,7 +118,12 @@ export default function DreamsForm() {
     return (
         <div className="flex flex-col main-content h-screen text-white">
             <h1 className="text-5xl text-center font-bold pb-5">Dream Details</h1>
+            <div className='border border-white border-1 rounded-xl p-2 mb-2'>
+                <h1 className="text-center text-3xl">The Dream</h1>
+                <p>{dream}</p>
+            </div>
             <div className="flex md:flex-row flex-col relative">
+
                 <div className="md:w-2/3 text-white border-white border rounded-xl">
                     <h1 className="text-3xl text-center font-bold pb-5">Interpretations</h1>
                     {loadingInterpretations && (
