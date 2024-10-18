01de6a62d6e959c4045b6a633b187644a07853f9
Michael Koch <michaelkoch4444@gmail.com>
2024-10-18 12:28:19 -0400

    Updates

--- src/components/DreamStream.jsx
+++ src/components/DreamStream.jsx
@@ -1,8 +1,6 @@
 "use client";
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
@@ -18,7 +16,7 @@ const DreamItem = ({ dream }) => {
     <div className="bg-[rgba(128,128,128,0.1)] rounded-xl overflow-hidden shadow-2xl mb-4 w-full md:w-1/2 mx-auto p-2">
       <Image
         src={dream.imageURL}
-        alt={dream.imageURL}
+        alt={"No Image Available"}
         className="w-full h-auto object-cover rounded-lg"
         layout="responsive"
         width={1}
@@ -26,7 +24,6 @@ const DreamItem = ({ dream }) => {
       />
       <p className="mt-2 golden-ratio-2">{new Date(dream.dreamDate).toLocaleDateString()}</p>
       <div className="p-4 pt-0">
-        
         <p className="text-white">
           {showFullText ? dream.dream : dream.dream.substring(0, 200)}
           {!showFullText && dream.dream.length > 200 && (
@@ -55,63 +52,43 @@ const DreamItem = ({ dream }) => {
 };
 
 const DreamStream = () => {
-  const [dreamStreamSymbols, setDreamStreamSymbols] = useState([]);
-  const [selectedSymbol, setSelectedSymbol] = useState(null);
-  const symbolsRef = useRef([]); // Use refs to target the elements for animation
   const [dreamStream, setDreamStream] = useState([]);
 
   useEffect(() => {
-    const fetchDreamStreamSymbols = async () => {
-      const res = await axios.get('/api/dream/symbols/dreamStreamSymbols');
-      setDreamStreamSymbols(res.data);
-    };
-
     const fetchPublicDreams = async () => {
-      const res = await axios.get('/api/dream/dreamStream');
-      setDreamStream(res.data.dreams);
+      try {
+        const res = await axios.get('/api/dream/dreamStream', {
+          headers: {
+            'Cache-Control': 'no-store', // Prevent caching
+          },
+        });
+        setDreamStream(res.data.dreams);
+      } catch (error) {
+        console.error('Error fetching dreams:', error);
+      }
     };
 
-    fetchDreamStreamSymbols();
+    // Fetch dreams initially
     fetchPublicDreams();
+
+    // Set up a polling interval to fetch the latest dreams every 10 seconds
+    const interval = setInterval(() => {
+      fetchPublicDreams();
+    }, 10000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="md:w-2/3 md:mx-auto md:px-0 md:py-8 px-3 py-8 bg-transparent">
       <h2 className="text-center mb-6 gradient-title-text golden-ratio-4">Dream Stream</h2>
-
-
       {/* Dream Stream section */}
       <div className="mt-8">
         {dreamStream.map((dream) => (
           <DreamItem key={dream._id} dream={dream} />
         ))}
       </div>
-
-      {/* Modal */}
-      {selectedSymbol && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          {/* Overlay */}
-          <div
-            className="fixed inset-0 bg-black opacity-50"
-            onClick={() => setSelectedSymbol(null)}
-          ></div>
-
-          {/* Modal Content */}
-          <div className="bg-white rounded-lg p-8 z-50 max-w-md mx-auto">
-            <h2 className="text-2xl font-bold mb-4 text-black text-center">
-              {selectedSymbol.symbol}
-            </h2>
-            <p className="text-gray-700 mb-6">{selectedSymbol.meaning}</p>
-            <p className="text-gray-600">Dreamt {selectedSymbol.count} times</p>
-            <button
-              onClick={() => setSelectedSymbol(null)}
-              className="mt-6 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
