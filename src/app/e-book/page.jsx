7582fab9bfbcbfc18cd232a34cc9e6e0323467c2
Michael Koch <michaelkoch4444@gmail.com>
2024-10-25 16:38:43 -0400

    Updates

---
+++ src/app/e-book/page.jsx
@@ -0,0 +1,6 @@
+"use client";
+import EBookForm from "@/components/EBookForm"
+
+export default function EBookPage() {
+    return (<EBookForm />)
+}
---
+++ src/components/EBookForm.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const EBookForm = () => {
+  
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-transparent text-white">
+          E-Book Page
+        </div>
+    );      
+  };
+  
+  export default EBookForm;
