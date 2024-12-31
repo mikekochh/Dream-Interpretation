f2141f1000489b34920801f48f2062b82dadaccd
Michael Koch <michaelkoch4444@gmail.com>
2024-12-30 20:22:39 -0500

    Added caching for dream symbols

--- src/app/api/dream/symbols/route.js
+++ src/app/api/dream/symbols/route.js
@@ -1,17 +1,33 @@
 import { NextResponse } from 'next/server';
 import { connectMongoDB } from '../../../../../lib/mongodb';
 import Symbol from '../../../../../models/symbols';
+import Redis from 'ioredis';
+
+// Initialize Redis client
+const redis = new Redis(); // Default host: localhost, port: 6379
 
 export async function GET(req) {
     try {
+        const cacheKey = 'dreamSymbols';
+
+        // Check if data is in Redis cache
+        const cachedSymbols = await redis.get(cacheKey);
+        if (cachedSymbols) {
+            console.log('Serving dream symbols from Redis');
+            return NextResponse.json(JSON.parse(cachedSymbols));
+        }
+
+        // Fetch data from MongoDB
+        console.log('Fetching dream symbols from database');
         await connectMongoDB();
-        
-        // Fetch dream symbols and sort by symbolsCount in descending order
         const dreamSymbols = await Symbol.find().sort();
-        
+
+        // Store data in Redis with 1-hour expiration
+        await redis.set(cacheKey, JSON.stringify(dreamSymbols), 'EX', 3600);
+
         return NextResponse.json(dreamSymbols);
     } catch (error) {
-        console.log('error getting dream symbols: ', error);
+        console.log('Error getting dream symbols:', error);
         return NextResponse.error(error);
     }
 }
